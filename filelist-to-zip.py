44cb3fab26daf4b29e7f5b5811b76b4e19d77e1f
Scott Petteway <scott@petteway.com>
2020-10-09 21:45:27 -0400

    no message

--- filelist-to-zip.py
+++ filelist-to-zip.py
@@ -40,7 +40,7 @@ if len(Req.errors)>0:
 	print(Req.errors)
 	quit(1)
 
-print("Found {x} records".format(x=len(Reg.jsonData)))
+print("Found {x} records".format(x=len(Req.jsonData)))
 for cl in Req.jsonData:
 	print('Starting '+cl['TRACKOR_KEY'])
 	try:
